d2d168692d7d0faf093cf06abe90c164cdce057c
iissnan <iissnan@gmail.com>
2015-11-17 14:31:54 +0800

    Add Getting Started Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
 
+
   $('#nav').affix({
     offset: {
       top: $('header').height()
     }
   });
 
-  $('header').css('background-image', getHeaderBackgroundImage());
+  $('.main header').css('background-image', getHeaderBackgroundImage());
 
   function getHeaderBackgroundImage () {
     var r = 1 + parseInt(Math.random() * 9, 10);
