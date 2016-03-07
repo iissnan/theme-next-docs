9d0938af763e8cecc28bf5ab4225f2ca4d8431fd
iissnan <iissnan@gmail.com>
2016-03-07 17:13:14 +0800

    Using Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,22 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var utils = require('./lib/utils');
 var renderer = require ('./lib/renderer');
+
+var source = {
+  files: {
+    html: './app/**/*.nun',
+    js: './app/assets/js/**/*.js',
+    css: './app/assets/css/**/*.css',
+    img: './app/assets/img/',
+    sass: './app/assets/sass/**/*.scss'
+  },
+  dirs: {
+    index: './app',
+    css: '/app/assets/css'
+  }
+};
 
 gulp.task('browser-sync', () => {
   browserSync.init({
@@ -20,12 +35,22 @@ gulp.task('browser-sync', () => {
   });
 
   gulp.watch([
-    './app/**/*.nun',
-    './app/assets/js/**/*.js',
-    './app/assets/css/**/*.css',
-    './app/assets/img/*'
+    source.files.html,
+    source.files.js,
+    source.files.css,
+    source.files.img
   ], browserSync.reload);
 });
 
+gulp.task('sass', () => {
+  return gulp.src(source.files.sass)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(source.dirs.css));
+});
 
-gulp.task('default', ['browser-sync']);
+gulp.task('sass:watch', () => {
+  gulp.watch(source.files.sass, ['sass']);
+});
+
+
+gulp.task('default', ['sass', 'sass:watch', 'browser-sync']);
