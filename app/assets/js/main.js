973af66b6e59f922154be929a0ab6c5296472399
iissnan <iissnan@gmail.com>
2016-03-08 15:14:19 +0800

    Update Styles

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
 
-  $('#nav').affix({
-    offset: {
-      top: $('header').height()
-    }
-  });
+  affix('#nav', $('header').height());
+  $('.next-docs-sidebar').size() > 0 && affix('.next-docs-sidebar', $('.next-docs-sections').offset().top);
 
   $('.home header').css('background-image', function getHeaderBackgroundImage() {
     var r = 1 + parseInt(Math.random() * 9, 10);
     return 'url("/assets/img/banner-' + r + '.jpg")';
   });
+
+  function affix (target, offset) {
+    $(target).affix({
+      offset: {
+        top: offset
+      }
+    });
+  }
 });
