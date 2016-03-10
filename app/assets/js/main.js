ae247df92c86dee9bdba13a48b503b66fb244f50
iissnan <iissnan@gmail.com>
2016-03-10 20:07:36 +0800

    Add code highlight section

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -4,8 +4,38 @@ $(document).ready(function () {
   $('.next-docs-sidebar').size() > 0 && affix('.next-docs-sidebar', $('.next-docs-sections').offset().top);
 
   $('.home header').css('background-image', function getHeaderBackgroundImage() {
-    var r = 1 + parseInt(Math.random() * 9, 10);
+    var r = 1 + parseInt(Math.random() * 8, 10);
     return 'url("/assets/img/banner-' + r + '.jpg")';
+  });
+
+  $('.next-doc-highlight-schemes').slick({
+    dots: true,
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 3,
+    initialSlide: 2,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-arrow-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fa fa-arrow-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0',
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 
   function affix (target, offset) {
